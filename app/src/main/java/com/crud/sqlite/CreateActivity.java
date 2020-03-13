2e17f9de10e3db1145dd5c3dfa96930b7f1a9ff1
Iwon Gunawan <iwongunawan@gmail.com>
2020-03-13 17:19:02 +0700

    added page create

--- app/src/main/java/com/crud/sqlite/CreateActivity.java
+++ app/src/main/java/com/crud/sqlite/CreateActivity.java
@@ -1,14 +1,89 @@
 package com.crud.sqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class CreateActivity extends AppCompatActivity {
+import com.crud.sqlite.utils.DBDataSource;
+import com.crud.sqlite.utils.Items;
+
+public class CreateActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText itemsName;
+    private EditText itemsBrand;
+    private EditText itemsPrice;
+    private Button btnCreate;
+
+    // data source
+    private DBDataSource dbDataSource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+
+        // toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Create New");
+        setSupportActionBar(toolbar);
+
+        // inisiasi
+        itemsName       = (EditText) findViewById(R.id.itemsName);
+        itemsBrand      = (EditText) findViewById(R.id.itemsBrand);
+        itemsPrice      = (EditText) findViewById(R.id.itemsPrice);
+        btnCreate       = (Button) findViewById(R.id.btnCreate);
+
+        dbDataSource = new DBDataSource(this); // inisiasi class DBDataSource.java
+        dbDataSource.open(); // connectiong db
+
+        btnCreate.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btnCreate) {
+            save();
+        }
+    }
+
+    public void save() {
+        // get text
+        String name   = itemsName.getText().toString();
+        String brand  = itemsBrand.getText().toString();
+        String price  = itemsPrice.getText().toString();
+
+        if (!isEmpty(name) && !isEmpty(brand) && !isEmpty(price)) {
+            // inisiasi barang baru masih kosong
+            Items items = null;
+            items = dbDataSource.create(name, brand, price);
+
+            if (!isEmpty(items.getItems_name())) {
+                Toast.makeText(this, "success :" + items, Toast.LENGTH_LONG).show();
+
+                itemsName.setText("");
+                itemsBrand.setText("");
+                itemsPrice.setText("");
+
+                // Log.d("INFO", "save: " + items);
+                // Log.d("INFO", "save: " + items.getItems_name());
+            }
+
+        }
+        else
+        {
+            Toast.makeText(this, "Data Can't be Empty!", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    public boolean isEmpty(String s) {
+        return TextUtils.isEmpty(s);
     }
 }
